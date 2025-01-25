ee17f03a4f05aed4f1feb9b3bd4d4fb575a66418
nomar <nomaraless@gmail.com>
2025-01-25 22:28:26 +0300

    initial commit

---
+++ src/main/java/com/shelter/shelter/ShelterApplication.java
@@ -0,0 +1,13 @@
+package com.shelter.nomaraless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomaralessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shelter/shelter/ShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelter.nomaraless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomaralessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
